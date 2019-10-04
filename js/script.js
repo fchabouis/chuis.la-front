b7c9473194cd46fa07df9e95a93283a023f5ab33
Francis <fchabouis@gmail.com>
2019-10-04 11:50:36 +0200

    a little bit of css and datetime of last position

--- js/script.js
+++ js/script.js
@@ -1,31 +1,34 @@
+function sharePosition() {
+  var interval = 5000
+  var identifiant;
+  var token;
+  console.log('coucou')
 
-var interval = 5000
-var identifiant;
-var token;
-console.log('coucou')
+  if ("geolocation" in navigator) {
+    const request = new Request('https://30556981.ngrok.io', { method: 'POST', body: `{}` });
+    fetch(request)
+      .then(response => {
+        console.log('identifiants recus !')
+        console.log(response)
+        identifiant = response.identifiant
+        token = response.token
+        document.getElementById('identifiant').innerHTML = `Réponse du serveur<br>identifiant: ${identifiant}<br>token: ${token}`
+      })
 
-if ("geolocation" in navigator) {
-
-  const request = new Request('https://30556981.ngrok.io', { method: 'POST', body: `{}` });
-  fetch(request)
-    .then(response => {
-      console.log('identifiants recus !')
-      identifiant = request.identifiant
-      token = request.token
-      document.getElementById('identifiant').innerHTML = request.identifiant + ' ' + request.token
-    })
-
-  setInterval(function () {
-    navigator.geolocation.getCurrentPosition((position) => {
-      console.log(position)      
-      var myPosition = {
-        latitude: position.coords.latitude,
-        longitude: position.coords.longitude
-      };
-      document.getElementById('position').innerHTML = myPosition.latitude + ', ' + myPosition.longitude
-      const request = new Request(`https://30556981.ngrok.io/${identifiant}`, { method: 'POST', body: `{token: ${token}, localisation: : ${position}}` });
-    });
-  }, interval);
-} else {
-  document.getElementById('position').innerHTML = 'Gélolocalisation indisponible'
+    setInterval(function () {
+      navigator.geolocation.getCurrentPosition((position) => {
+        console.log(position)
+        var myPosition = {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        };
+        document.getElementById('position').innerHTML = myPosition.latitude + ', ' + myPosition.longitude
+        document.getElementById('dateDernierePosition').innerHTML = new Date();
+        
+        const request = new Request(`https://30556981.ngrok.io/${identifiant}`, { method: 'POST', body: `{token: ${token}, localisation: : ${position}}` });
+      });
+    }, interval);
+  } else {
+    document.getElementById('position').innerHTML = 'Gélolocalisation indisponible'
+  }
 }
